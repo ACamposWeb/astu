5744411d8bc6d9ae0282f9ca7c986b0f571a94e9
ACamposWeb <sc19013@ues.edu.sv>
2025-05-25 18:43:22 -0600

    Subiendo cambios HomeController y AstuApplication

--- src/main/java/com/g9/astu/astu/OtherController.java
+++ src/main/java/com/g9/astu/astu/OtherController.java
@@ -1,13 +1,9 @@
 package com.g9.astu.astu;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class OtherController {
 
-    @GetMapping("/")
-    public String home() {
-        return "index";  // Spring buscará src/main/resources/templates/index.html
-    }
 }
